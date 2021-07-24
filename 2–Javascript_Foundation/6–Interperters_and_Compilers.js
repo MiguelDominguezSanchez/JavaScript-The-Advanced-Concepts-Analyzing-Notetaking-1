41f46b589932fc54755d83298c4b5f1d3c20aae8
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-07-24 14:34:29 +0200

    #2 6–Interprerters_and_Compilers Fragment

---
+++ 2–Javascript_Foundation/6–Interperters_and_Compilers.js
@@ -0,0 +1,25 @@
+// Interpreters and Compiler
+function someCalculation(x, y) {
+	return x + y
+}
+
+for (let i = 0; i < 1000; i++) {
+	someCalculation(5, 4)
+}
+
+// console
+9
+
+/*
+(Code Above) i have some code here that well doesn't do very much, Does it?
+I have a function that does some calculation, and we just loop over this calculation, which is five plus four, right over here 'return x + y', and we do that a thousand times, thats a long time..
+And you  see that we get the result over here.
+But the loop is happening over and over 
+
+1'10''
+
+*/
+
+/*
+(back to html script)
+*/
